2bbdfeacfae22fc7438b8145e0420706798198eb
Raúl González Vicente <rgonzale@viewnext.com>
2017-12-27 23:29:43 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,30 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+// GET /todos/123245
+ // id disponible en el objeto request
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  // valid id using isValis
+  if (!ObjectID.isValid(id)) {
+    // 404 - send back empty send
+    return res.status(404).send();
+  }
+
+  // findById
+  Todo.findById(id).then((todo) => {
+    // success
+    if (!todo) {
+      // if no todo - 404 with empty body
+      return res.status(404).send();
+    }
+      // if todo - send it back
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
