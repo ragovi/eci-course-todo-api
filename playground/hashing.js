11a9634cadc2d80786406f6d92d084b027b9c0d5
Raúl González Vicente <rgonzale@viewnext.com>
2017-12-29 01:14:40 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,33 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// primer argumento es un numero de rounds para generar el salt. Cuanto más alto mas tarda
+// bcrypt es un algoritmo lento, lo que es bueno para prevenor ataques por fuerza brute
+// hay quien usa 120 rounds
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$BRky1heQXLdr9NQ6tnQVxO3MmLEIY4sYh41eNW8DQRUDvl/1ZHbSa';
+
+bcrypt.compare('123!', hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 // 256 es el numero de bits que tendrá el hash
